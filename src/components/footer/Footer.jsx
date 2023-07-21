cc1ae39afbbb7b5d3fec265b94ee8ced4089fcc3
hind-hisham <hind.hisham.alsadig@gmail.com>
2023-07-21 07:20:05 +0300

    a

--- src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './footer.css'
-const Footer = () => {
-  return (
-    <p className="copy-right">✨This Websit Design and Devolope By Hind hisham @ 2022</p>
-  )
-}
-
-export default Footer
